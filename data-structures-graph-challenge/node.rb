1eb3899cc5b8f86b332153f53b474d341e6cbf34
ray-curran <rpcurr@gmail.com>
2016-03-29 09:34:27 -0500

    Add graph challenge

--- data-structures-graph-challenge/node.rb
+++ data-structures-graph-challenge/node.rb
@@ -1,3 +1,21 @@
 class Node
+  attr_reader :value, :nodes
+
+  def initialize(value)
+    @value = value
+    @nodes = []
+  end
+
+  def add_edge(other_node)
+    @nodes << other_node
+  end
+
+  def exists?(&block)
+    @nodes.each do |node|
+      return true if yield(node)
+      return true if node.exists?(&block) == true
+    end
+    false
+  end
 
 end
--- data-structures-graph-challenge/node_spec.rb
+++ data-structures-graph-challenge/node_spec.rb
@@ -1,5 +1,54 @@
 require_relative 'node'
 
 describe Node do
+  let(:node) {Node.new('hai')}
+
+  describe '#initialize' do
+    it 'has an assigned value' do
+     expect(node.value).to eq 'hai'
+   end
+
+   it 'initializes with a blank array of edges' do
+    expect(node.nodes.length).to eq 0
+   end
+ end
+
+ describe '#add_edge' do
+  it 'increases the number of edges' do
+    expect{node.add_edge(Node.new('bai'))}.to change{node.nodes.length}.from(0).to(1)
+  end
+
+  it 'adds the appropriate element as an edge for the new node' do
+    node.add_edge(Node.new('bai'))
+    expect(node.nodes[0].value).to eq 'bai'
+  end
+ end
+
+  let(:matt) {Node.new('matt')}
+  let(:mike) {Node.new('mike')}
+  let(:duke) {Node.new('duke')}
+  let(:ray) {Node.new('ray')}
+  let(:alyssa) {Node.new('alyssa')}
+
+  before do
+    matt.add_edge(mike)
+    mike.add_edge(ray)
+    mike.add_edge(duke)
+    duke.add_edge(alyssa)
+  end
+
+ describe '#exists' do
+  it 'finds the right node when the node edges the other' do
+    expect(matt.exists?{|node| node.value == 'mike'}).to be true
+  end
+
+  it 'finds the right node when the node is 3 away' do
+    expect(matt.exists?{|node| node.value == 'alyssa'}).to be true
+  end
+
+  it 'does not find the node if it only edges that one direction' do
+    expect(mike.exists?{|node| node.value == 'matt'}).to be false
+  end
+ end
 
 end
